8a2c0a418824c2ba945b42f64cd1edcedc983e80
Shahzaib <ranashahzaib2025@gmail.com>
2025-03-11 20:44:11 +0500

    Book Scraping

--- bookscrape.py
+++ bookscrape.py
@@ -8,3 +8,18 @@ r = requests.get(url)
 
 soup = BeautifulSoup(r.text, "html.parser")
 
+catalogue = soup.select("article",class_= "product_prod")
+
+for book in catalogue:
+    title = book.h3.a['title']
+    price = book.find("p",class_="price_color")
+    rating = book.find("p", class_ = "star-rating")["class"][1]
+    availaibility = book.find("p", class_ = "instock availaibility")
+
+    
+print("Title:" , title)
+print("Price", price)
+print("rating:", rating)
+print("Availaible:", availaibility)
+
+# print(soup.prettify()) 
